4e8cb70016f69ba201adad919e2b9712f48f1686
pipeline <pipeline@nuance.com>
2023-04-10 12:19:25 +0530

    criteria API from and where clause

---
+++ src/main/java/com/atom/hibernatebasics/criteria/CriteriaTest.java
@@ -0,0 +1,22 @@
+package com.atom.hibernatebasics.criteria;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CriteriaTest {
+    public static void main(String[] args){
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        EmployeeDao employeeDao = new EmployeeDao();
+
+        employeeDao.fillEmployeeData(session);
+        employeeDao.displayAllEmployees(session);
+        employeeDao.whereExample(session);
+
+        session.getTransaction().commit();
+        session.close();
+        sessionFactory.close();
+    }
+}
---
+++ src/main/java/com/atom/hibernatebasics/criteria/EmployeeDao.java
@@ -0,0 +1,58 @@
+package com.atom.hibernatebasics.criteria;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class EmployeeDao {
+
+    public void fillEmployeeData(Session session) {
+        System.out.println("--Insert employee--");
+        for(int i=1; i<= 10 ; i++){
+            Employee employee = new Employee();
+            employee.setName("Employee "+i);
+            employee.setDepartment("Department "+ i % 2);
+            session.persist(employee);
+        }
+    }
+    public void displayAllEmployees(Session session) {
+        System.out.println("\n--displayAllEmployees--");
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<Employee> criteriaQuery  = criteriaBuilder.createQuery(Employee.class);
+        Root<Employee> from = criteriaQuery.from(Employee.class);
+        criteriaQuery.select(from);
+        Query<Employee> query = session.createQuery(criteriaQuery);
+        List<Employee> list = query.list();
+        printEmployeeList(list);
+
+    }
+
+    private static void printEmployeeList(List list) {
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()){
+            Employee employee = (Employee) iterator.next();
+            System.out.print("["+employee.getName()+",");
+            System.out.print(employee.getDepartment()+"]");
+        }
+        System.out.println("");
+        System.out.println("Size: "+list.size());
+    }
+
+    public void whereExample(Session session) {
+        System.out.println("\n--whereExample--");
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+        Root<Employee> from = criteriaQuery.from(Employee.class);
+        criteriaQuery.select(from).where(criteriaBuilder.equal(from.get("name"), "Employee 2"));
+        Query<Employee> query = session.createQuery(criteriaQuery);
+        List<Employee> list = query.list();
+        printEmployeeList(list);
+    }
+}
